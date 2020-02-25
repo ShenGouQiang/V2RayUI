d7c01790d2ec024a221295131d300682e3fb7a37
沈构强 <shengouqiang@shengouqiangdegongzuoben.local>
2020-02-25 11:50:29 +0800

    首次初始化项目

---
+++ src/main/java/com/gouqiang/shen/v2rayui/V2rayUiApplication.java
@@ -0,0 +1,13 @@
+package com.gouqiang.shen.v2rayui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class V2rayUiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(V2rayUiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gouqiang/shen/v2rayui/V2rayUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gouqiang.shen.v2rayui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class V2rayUiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

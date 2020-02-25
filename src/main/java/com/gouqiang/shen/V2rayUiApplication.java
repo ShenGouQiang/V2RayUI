85cd71e29932a3f9ee9714ac53292bfdd9e12451
沈构强 <shengouqiang@shengouqiangdegongzuoben.local>
2020-02-25 12:12:51 +0800

    结构化代码工程

--- src/main/java/com/gouqiang/shen/V2rayUiApplication.java
+++ src/main/java/com/gouqiang/shen/V2rayUiApplication.java
@@ -1,4 +1,4 @@
-package com.gouqiang.shen.v2rayui;
+package com.gouqiang.shen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/gouqiang/shen/controller/WebController.java
@@ -0,0 +1,11 @@
+package com.gouqiang.shen.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author shengouqiang
+ * @date 2020/2/25
+ */
+@RestController
+public class WebController {
+}
